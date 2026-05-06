9843ea198c9c244f142fe7075353d0eb46a1214f
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-05-06 22:22:16 +0200

    init: configure app

---
+++ README.md
@@ -0,0 +1,3 @@
+The main idea:
+
+=> create a tracker of subscription costs with info on ongoing payment plans, total costs, payment schedule, etc. Then - add modules for obligatory payments for tracking (like rent, electricity, mobile plan, etc).
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './assets/pages/Home'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
