551a92a2ca4271f331129c528ee94f150c44bd67
Anastasiya K <95680560+hamsterK@users.noreply.github.com>
2026-05-15 16:30:04 +0200

    feat: implement footer with contact info (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './assets/pages/Home'
 import Header from './assets/pages/Header'
+import Footer from './assets/pages/Footer'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   )
 }
