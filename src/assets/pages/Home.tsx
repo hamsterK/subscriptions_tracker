c2535f560aece3f1f933fe0c38cbc1e338475709
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2026-05-29 10:49:28 +0200

    feat: update Home component with temp mock data

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <>
       <h1>Welcome</h1>
-      <SubscriptionComponent subscription={mockState.subscriptions} />
-    </>
+      <SubscriptionComponent
+        subscription={mockState.subscriptions[0]}
+      />    </>
   )
 }
